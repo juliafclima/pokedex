00d21699b1a8f85eb2f308bd1bde0bc5ff298d2b
Júlia Lima <juliafclima@hotmail.com>
2024-01-31 18:53:12 -0300

    feat: buscando pokemon pelo nome

--- src/js/details.js
+++ src/js/details.js
@@ -1,38 +1,37 @@
-const pokemonDetailsContainer = document.getElementById("pokemonDetails");
+document.addEventListener("DOMContentLoaded", () => {
+  const pokemonDetailsContainer = document.getElementById("pokemonDetails");
 
-function carregarDetalhesPokemon() {
+  // Get Pokemon name from URL
   const params = new URLSearchParams(window.location.search);
-  
-  const pokemonId = params.get("id");
-  console.log(pokemonId);
-  
-  if (pokemonId) {
-    
-    pokeapi
-      .getPokemonsDetail({
-        url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`,
-      })
-      .then((pokemon) => {
+  const pokemonName = params.get("name");
 
-        pokemonDetailsContainer.innerHTML = `
-          <div class="pokemon ${pokemon.types[0]}" data-id="${pokemon.order}">
-            <span class="number">Número: #00${pokemon.order}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-              <div class="types">
-                ${pokemon.types
-                  .map((type) => `<span class="type ${type}">${type}</span>`)
-                  .join("")}
-              </div>
-              <img src="${pokemon.image}" alt="${pokemon.name}" />
-            </div>
-          </div>
-        `;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-}
+  // Fetch and display Pokemon details
+  pokeapi.getPokemonsDetailByName(pokemonName).then((pokemon) => {
+    const detailHtml = `
+    <div class="pokemon ${pokemon.types[0]}" data-id="${pokemon.order}">
+    <span class="number">Número: #00${pokemon.order}</span>
+    <span class="name">${pokemon.name}</span>
+    <div class="detail">
+      <div class="types">
+        ${pokemon.types
+          .map((type) => `<span class="type ${type}">${type}</span>`)
+          .join("")}
+      </div>
+      <img src="${pokemon.image}" alt="${pokemon.name}" />
+    </div>
+  </div>
+    `;
+    pokemonDetailsContainer.innerHTML = detailHtml;
+  });
+});
 
-document.addEventListener("DOMContentLoaded", carregarDetalhesPokemon);
+pokeapi.getPokemonsDetailByName = (pokemonName) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`;
+
+  return fetch(url)
+    .then((response) => response.json())
+    .then(converterPokeApiDetailPokemon)
+    .catch((error) => {
+      console.error(error);
+    });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,61 @@
-const pokemonList = document.getElementById("pokemonList");
-const botaoMore = document.getElementById("loadMoreBtn");
+document.addEventListener("DOMContentLoaded", () => {
+  const pokemonList = document.getElementById("pokemonList");
+  const botaoMore = document.getElementById("loadMoreBtn");
+  const maxRecords = 151;
+  const limite = 26;
+  let offset = 0;
 
-const maxRecords = 151;
-const limite = 26;
-let offset = 0;
+  function exibirMais(offset, limite) {
+    pokeapi.getPokemons(offset, limite).then((pokemons = []) => {
+      const newList = pokemons
+        .map(
+          (pokemon) => `
+            <li class="pokemon ${pokemon.types[0]}" data-name="${pokemon.name}">
+              <span class="number">#00${pokemon.order}</span>
+              <span class="name">${pokemon.name}</span>
+              <div class="detail">
+                <ol class="types">
+                  ${pokemon.types
+                    .map((type) => `<li class="type ${type}">${type}</li>`)
+                    .join("")}
+                </ol>
+                <img src="${pokemon.image}" alt="${pokemon.name}" />
+              </div>
+            </li>`
+        )
+        .join("");
 
-function exibirMais(offset, limite) {
-  
-  pokeapi.getPokemons(offset, limite).then((pokemons = []) => {
-    const newList = pokemons
-      .map(
-        (pokemon) => `
-          <li class="pokemon ${pokemon.types[0]}" data-id="${pokemon.order}">
-            <span class="number">#00${pokemon.order}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-              <ol class="types">
-                ${pokemon.types
-                  .map((type) => `<li class="type ${type}">${type}</li>`)
-                  .join("")}
-              </ol>
-              <img src="${pokemon.image}" alt="${pokemon.name}" />
-            </div>
-          </li>`
-      )
-      .join("");
+      if (pokemonList) {
+        pokemonList.innerHTML += newList;
 
-    if (pokemonList) {
-      pokemonList.innerHTML += newList;
+        const pokemonItems = document.querySelectorAll(".pokemon");
 
-      const pokemonItems = document.querySelectorAll(".pokemon");
-
-      pokemonItems.forEach((item) => {
-        item.addEventListener("click", () => {
-          const pokemonId = item.dataset.id;
-          window.location.href = `details.html?id=${pokemonId}`;
+        pokemonItems.forEach((item) => {
+          item.addEventListener("click", () => {
+            const pokemonName = item.dataset.name;
+            window.location.href = `details.html?name=${pokemonName}`;
+          });
         });
-      });
-      
+      } else {
+        console.error("Elemento pokemonList não encontrado.");
+      }
+    });
+  }
+
+  exibirMais(offset, limite);
+
+  botaoMore.addEventListener("click", () => {
+    offset += limite;
+
+    const qntRecordsProximaPagina = offset + limite;
+
+    if (qntRecordsProximaPagina >= maxRecords) {
+      const newLimite = maxRecords - offset;
+      exibirMais(offset, newLimite);
+
+      botaoMore.parentElement.removeChild(botaoMore);
     } else {
-      console.error("Elemento pokemonList não encontrado.");
+      exibirMais(offset, limite);
     }
   });
-}
-
-exibirMais(offset, limite);
-
-botaoMore.addEventListener("click", () => {
-  offset += limite;
-
-  const qntRecordsProximaPagina = offset + limite;
-
-  if (qntRecordsProximaPagina >= maxRecords) {
-    const newLimite = maxRecords - offset;
-    exibirMais(offset, newLimite);
-
-    botaoMore.parentElement.removeChild(botaoMore);
-  } else {
-    exibirMais(offset, limite);
-  }
 });
