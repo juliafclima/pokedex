fea10f076e72ed25c12bf88c8ac97929c8793146
Júlia Lima <juliafclima@hotmail.com>
2024-02-01 14:36:31 -0300

    feat: style card detalhe

--- src/js/details.js
+++ src/js/details.js
@@ -1,23 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const pokemonDetailsContainer = document.getElementById("pokemonDetails");
 
-  // Get Pokemon name from URL
   const params = new URLSearchParams(window.location.search);
   const pokemonName = params.get("name");
 
-  // Fetch and display Pokemon details
   pokeapi.getPokemonsDetailByName(pokemonName).then((pokemon) => {
     const detailHtml = `
-    <div class="pokemon ${pokemon.types[0]}" data-id="${pokemon.order}">
-    <span class="number">Número: #00${pokemon.order}</span>
-    <span class="name">${pokemon.name}</span>
-    <div class="detail">
-      <div class="types">
-        ${pokemon.types
-          .map((type) => `<span class="type ${type}">${type}</span>`)
-          .join("")}
-      </div>
-      <img src="${pokemon.image}" alt="${pokemon.name}" />
+    <div class="card ${pokemon.types[0]}" data-id="${pokemon.order}">
+    <span>#00${pokemon.order}</span>
+    <h1>${pokemon.name}</h1>
+    <img
+      src="${pokemon.image}"
+      alt="${pokemon.name}"
+    />
+    <div class="pokemon-tipos">
+      ${pokemon.types
+        .map((type) => `<span class="type ${type}">${type}</span>`)
+        .join("")}
     </div>
   </div>
     `;
